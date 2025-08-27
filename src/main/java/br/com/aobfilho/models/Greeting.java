268d16d5a971753398f706e921f29d716d517a11
Antonio Brandão <antonioobfilho@gmail.com>
2025-08-26 21:54:13 -0300

    init: first commit

---
+++ src/main/java/br/com/aobfilho/models/Greeting.java
@@ -0,0 +1,3 @@
+package br.com.aobfilho.models;
+
+public record Greeting (long id, String context) {}
---
+++ src/test/java/br/com/aobfilho/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.aobfilho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
